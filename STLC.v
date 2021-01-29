ce2ea57d5d9d99d015d8d99354460bf073b5a56f
MisakaCenter <misaka@sjtu.edu.cn>
2021-01-29 23:48:52 +0800

    Eval is ready.

--- STLC.v
+++ STLC.v
@@ -10,7 +10,8 @@ Require Import Coq.Arith.Peano_dec.
 Inductive ty : Type :=
   | tyNat
   | tyBool
-  | tyArrow (t1:ty) (t2:ty).
+  | tyArrow (t1:ty) (t2:ty)
+  | Error.
 
 Inductive tm : Type :=
   | const : string -> tm
@@ -66,24 +67,91 @@ Definition rho_const: list (string * ty) :=
   [("S" , tyArrow tyNat tyNat);
    ("O" , tyNat);
    ("true" , tyBool);
-   ("false" , tyBool)
+   ("false" , tyBool);
+   ("Type Error" , Error);
+   ("Unknown Error" , Error)
   ].
 
-Definition type_checker (t:tm) := type_check rho_const nil t.
+Definition type_checker (t: tm) := type_check rho_const nil t.
 
-Fixpoint tm_subst (t: tm)
+Fixpoint free_var_count (s: string)(t: tm): nat :=
+  match t with
+  | var s' => if string_dec s s' then 1 else 0
+  | abs s' typ t' => if string_dec s s' then 0 else free_var_count s t'
+  | app t1 t2 => (free_var_count s t1) + (free_var_count s t2)
+  | _ => 0
+  end.
+
+Definition free_var_dec (s: string)(t: tm): bool :=
+  match (free_var_count s t) with
+  | O => false
+  | _ => true
+  end.
+
+Fixpoint var_subst (l: list (string * tm)) (x: string) :=
+match l with
+| nil => var x
+| (x',t') :: l => if string_dec x' x then t' else var_subst l x
+end.
+
+Fixpoint tm_subst (l: list (string * tm))(t: tm) :tm :=
+  match t with
+  | const s => const s
+  | var s => var_subst l s
+  | app t1 t2 => app (tm_subst l t1) (tm_subst l t2)
+  | abs s typ t1 => 
+  (fix abs_subst (l: list (string * tm)):=
+  match l with
+  | nil => abs s typ t
+  | (x',t') :: l => if string_dec x' s
+                        then abs_subst l
+                        else if free_var_dec s t1
+                                then abs "ss" typ (tm_subst ((s, var "ss") :: l) t1)
+                                else abs s typ (tm_subst l t1)
+  end) l
+  end.
 
 Fixpoint next_state (t: tm): option tm :=
   match t with
-  | const _ _ => None
+  | const _ => None
   | var _ => None
   | abs _ _ _ => None
+  | app (abs s typ t1) t2 => match is_const_hd_val t2 with
+                         | None => Some (tm_subst [(s,t2)] t1)
+                         | Some _ => match next_state t2 with
+                                     | Some t2' => Some (app (abs s typ t1) t2')
+                                     | None => None
+                                     end
+                         end
   | app t1 t2 => match next_state t1 with
-                 | Some t' => next_state (app t' t2)
+                 | Some t' => Some (app t' t2)
                  | None => match next_state t2 with
-                           | Some t' => next_state t1 t'
+                           | Some t' => Some (app t1 t')
                            | None => Some (app t1 t2)
                            end
                  end
-  | app (abs s t1) t2 => Some (tm)
   end.
+
+Fixpoint multi_state_pre (limit: nat) (t: tm): list tm :=
+  match limit with
+  | O => nil
+  | S n => 
+    match next_state t with
+    | Some t' => t' :: (multi_state_pre n t')
+    | None => nil
+    end
+  end.
+
+Definition default_limit :nat := 10000.
+
+Definition multi_state (t: tm) := head(multi_state_pre default_limit t).
+
+Definition run (t: tm): tm :=
+  match type_checker t with
+  | None => const "Type Error"
+  | Some TYPE => 
+    match multi_state t with
+    | None => const "Unknown Error"
+    | Some t' => t'
+    end
+  end. 
