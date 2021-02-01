b38fb19fa60388ebcb68f48862a72e55435e7689
MisakaCenter <misaka@sjtu.edu.cn>
2021-02-02 00:02:04 +0800

    Simple REPL.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,20 @@
 module Main where
 import STLC ( Tm(Const, Abs, App, Var), Ty(TyNat), run )
+import Parser ( stlcParser )
+import Text.Parsec ( parse )
+import Text.Read ( Lexeme(String) )
+import Control.Monad
+import System.IO
 
-main :: IO()
-main = do let x = App (Abs "x" TyNat (App (Const "S") (Var "x"))) (Const "O")
-          print (run x)
+-- test case: app (abs.x:(Nat)=>(app (const S) (var x))) (const O)
+
+main :: IO() 
+main = do 
+          putStr "λλλλ>"
+          hFlush stdout
+          x <- getLine
+          unless (x == ":q") $ do
+            let parsed = parse stlcParser "" x
+            case parsed of Right parsedTm -> print (run parsedTm)
+                           Left warning -> print warning
+            main
